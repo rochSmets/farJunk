46174037ee52fb076ad861eebe58faef85a09179
roch smets <roch.smets@gmail.com>
2025-04-04 15:22:42 +0200

    wip

--- shells/cholesky/module.sh
+++ shells/cholesky/module.sh
@@ -1,5 +1,6 @@
-module load cmake/3.19.7
-module load cc/10.2.0
-module load penmpi/4.1.0
-module load df5/1.12.1
-module load ambaforge/22.11.1-4
+module load cmake/3.26.3
+module load gcc/10.2.0
+module load git/2.31.1
+module load openmpi/4.1.0
+module load hdf5/1.12.1
+module load mambaforge/22.11.1-4 
