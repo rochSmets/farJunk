9cfdb2a640450cce0741e2149a074b26f4bda3ea
roch smets <roch.smets@gmail.com>
2026-09-23 21:36:54 +0200

    up

--- shells/krusty/make.sh
+++ shells/krusty/make.sh
@@ -8,7 +8,7 @@ if [[ $build_type == debug ]]; then
     -DwithCaliper=OFF \
     -DtestMPI=OFF \
     -Dasan=OFF \
-    -DdevMode=ON \
+    -DdevMode=OFF \
     "$src_dir"
 else
   cmake -DCMAKE_CXX_FLAGS="-g3 -O3 -march=native -mtune=native -DPHARE_DIAG_DOUBLES=1" \
